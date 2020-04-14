0230e29fd13bc7f1f2a1c7c0fe98b3ab3e533329
Matthew Malecki <mmalecki4@gmail.com>
2020-04-14 21:29:09 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,21 @@
 # don't forget to add: require 'pry'
 
+
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    string = "s"+"s"+"s"+"s"+"s"+"s"+"s"+"s"+"s"+"s"+string
+#    binding.pry
+    string
   else
     string
   end
 end
+
+
+#def snake_it_up(string)
+#  if string[0] == "s"
+#    10 * "s" + string
+#  else
+#    string
+#  end
+#end
