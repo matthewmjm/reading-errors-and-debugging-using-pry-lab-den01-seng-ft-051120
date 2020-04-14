0ade7aaef4f9846fef9715f183a002a7e29662fa
Matthew Malecki <mmalecki4@gmail.com>
2020-04-14 19:57:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -17,9 +17,10 @@ require 'pry'
 
 def generate_star_date
   star_date = rand(100000) + 400000 / 10.0
+  binding.pry
   star_date
 end
-binding.pry
+
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
